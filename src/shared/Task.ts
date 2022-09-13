62e59803e0bb375c7cf5945df9aa790d6a8f0c7d
Noam Honig <noam.honig@gmail.com>
2022-09-13 08:55:38 +0300

    updated indent to highlight differences

--- src/shared/Task.ts
+++ src/shared/Task.ts
@@ -3,24 +3,24 @@ import { Allow, Entity, Fields, Validators } from "remult";
 import { Roles } from "./Roles";
 
 @Entity<Task>("tasks", {
-    allowApiRead: Allow.authenticated,
-    allowApiUpdate: Allow.authenticated,
-    allowApiInsert: Roles.admin,
-    allowApiDelete: Roles.admin
+      allowApiRead: Allow.authenticated,
+      allowApiUpdate: Allow.authenticated,
+      allowApiInsert: Roles.admin,
+      allowApiDelete: Roles.admin
 })
 export class Task {
-    constructor() {
-        makeAutoObservable(this);
-    }
-    @Fields.uuid()
-    id!: string;
+      constructor(){
+         makeAutoObservable(this);
+      }
+      @Fields.uuid()
+      id!: string;
 
-    @Fields.string({
-        validate: Validators.required,
-        allowApiUpdate: Roles.admin
-    })
-    title = '';
+      @Fields.string({
+         validate: Validators.required,
+         allowApiUpdate: Roles.admin
+      })
+      title = '';
 
-    @Fields.boolean()
-    completed = false;
+      @Fields.boolean()
+      completed = false;
 }
