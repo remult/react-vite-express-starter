f703f30d4c21f2fbdeac36f0d4181107e12e469a
Noam Honig <noam.honig@gmail.com>
2022-09-13 08:51:51 +0300

    refactored store to a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { remult } from "remult";
-import { Task } from "./shared/Task";
-import { TasksController } from "./shared/TasksController";
+import { useEffect } from "react";
 import { observer } from "mobx-react-lite";
-import { action, makeAutoObservable, observable, runInAction } from 'mobx';
+import { action } from 'mobx';
+import { Store } from "./Store";
+
+export const store = new Store();
 
 
-
-
-class Store {
-  taskRepo = remult.repo(Task);
-  tasks: Task[] = [];
-  hideCompleted = false;
-  constructor() {
-    makeAutoObservable(this);
-  }
-  replaceTasks(tasks: Task[]) {
-    this.tasks = tasks;
-  }
-  async loadTasks() {
-    this.replaceTasks(await
-      this.taskRepo.find({
-        limit: 20,
-        orderBy: { completed: "asc" },
-        where: { completed: this.hideCompleted ? false : undefined }
-      }));
-  }
-  addTask() {
-    this.tasks.push(new Task())
-  }
-  async saveTask(task: Task) {
-    try {
-      const savedTask = await this.taskRepo.save(task);
-      runInAction(() => store.tasks[store.tasks.indexOf(task)] = savedTask);
-    } catch (error: any) {
-      alert(error.message);
-    }
-  }
-  async deleteTask(task: Task) {
-    await this.taskRepo.delete(task);
-    runInAction(() => this.tasks = this.tasks.filter(t => t !== task));
-  }
-  async setAll(completed: boolean) {
-    await TasksController.setAll(completed);
-    this.loadTasks();
-  }
-}
-const store = new Store();
-
-function App() {
-  return <Todo store={store} />
-
-}
-const Todo = observer(({ store }: { store: Store }) => {
+const App = observer(() => {
   useEffect(() => {
     store.loadTasks()
   }, [store.hideCompleted]);
@@ -77,14 +31,12 @@ const Todo = observer(({ store }: { store: Store }) => {
             </div>
           );
         })}
-
       </main>
       <button type="button" onClick={() => store.addTask()}>Add Task</button>
       <div>
         <button type="button" onClick={() => store.setAll(true)}>Set all as completed</button>
         <button type="button" onClick={() => store.setAll(false)}>Set all as uncompleted</button>
       </div>
-
     </div>
   );
 });
