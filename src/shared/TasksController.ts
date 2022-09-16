0773cf8fdcbd3b8e5f876825dd0f14e71c74c807
Noam Honig <noam.honig@gmail.com>
2022-09-16 09:03:17 +0300

    working without decorators

--- src/shared/TasksController.ts
+++ src/shared/TasksController.ts
@@ -1,14 +1,18 @@
-import { Allow, BackendMethod, remult } from "remult";
+import { Allow, BackendMethod, describeClass, remult } from "remult";
 import { Task } from "./Task";
 import { Roles } from "./Roles";
 
 export class TasksController {
-   @BackendMethod({ allowed: Roles.admin })
+
    static async setAll(completed: boolean) {
       const taskRepo = remult.repo(Task);
 
       for (const task of await taskRepo.find()) {
-            await taskRepo.save({ ...task, completed });
+         await taskRepo.save({ ...task, completed });
       }
    }
 }
+
+describeClass(TasksController, undefined, undefined, {
+   setAll: BackendMethod({ allowed: Roles.admin })
+})
