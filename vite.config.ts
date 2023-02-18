35bb78da1e2bd452dbf4a18bb4cd2208d72003b5
Noam Honig <noam.honig@gmail.com>
2023-02-18 18:41:39 +0200

    updated for remult 18 tutorial

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3002'
-      }
-    }
-  }
+  server: { proxy: { "/api": "http://localhost:3002" } }
 })
