0841d10d3cf33b486c4a2338dc00a294bc58e486
Noam Honig <noam.honig@gmail.com>
2024-04-28 12:29:31 +0300

    fixed for esm

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { "/api": "http://localhost:3002" } }
-})
+  server: { proxy: { "/api": "http://localhost:3002" } },
+});
